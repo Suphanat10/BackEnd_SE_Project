b2e5902362d35728a73aaf61c35f07e5b88faafa
jamese <piyabutr10@gmail.com>
2024-03-19 01:43:40 +0700

    Update course_id to exam_id in course_exam_problem query

--- app/controllers/exam.controller.js
+++ app/controllers/exam.controller.js
@@ -609,7 +609,7 @@ exports.do_Exam = async (req, res) => {
       where: {
         registration_id: registration_id,
         course_exam_problem:{
-          course_id : examData.course_id
+          exam_id : examData.exam_id
         
         }
       },
