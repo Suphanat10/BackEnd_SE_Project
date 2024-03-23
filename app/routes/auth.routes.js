50482a6ce61abb4fc6ed5cf8161c5f8a775def94
jamese <piyabutr10@gmail.com>
2024-03-23 23:48:24 +0700

    Fix formatting issues in score.controller.js and auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -27,5 +27,7 @@ module.exports = function (app) {
 
   app.post("/api/login/google", controller.login_by_google);
 
-  app.post("/api/logout", controller.logout);
+  app.post("/api/logout",
+    
+   controller.logout);
 };
